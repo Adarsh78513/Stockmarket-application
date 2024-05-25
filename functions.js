f209fa749011506ed2868a74d224932628957723
A.R. Shivam <adrsrj@gmail.com>
2024-05-25 19:07:16 +0530

    Functions

--- functions.js
+++ functions.js
@@ -1,5 +1,7 @@
+const xlsx = require('xlsx');
 const fs = require('fs');
 const readline = require('readline');
+const csv = require('csv-parser');
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
@@ -82,8 +84,105 @@ async function generateText(generativeAI, prompt) {
     console.log("Text generated successfully");
 }
 
+
+financialFunctions = {
+    // 1. Quaterly financial results
+    // 2. Current date and quarter
+    // 3. Time remaining for the quater to end
+    // 4. Company symbol
+    currentDateAndQuarter: async () => {
+        const now = new Date();
+        const month = now.getMonth();
+        const quarter = Math.floor((month + 3) / 3);
+        const year = now.getFullYear();
+        return { year: year, quarter: quarter , date: now.toDateString().split(" ").slice(1).join(" ")};
+    },
+    companySymbols: async () => {
+        // Get the company symbol from the company name
+        // Using the file form the NSE website that lsits all the companies according to their market cap
+        const sheets = xlsx.readFile('src/MCAP28032024.xlsx');
+        const sheet = sheets.Sheets[sheets.SheetNames[0]];
+        const data = xlsx.utils.sheet_to_json(sheet, { header: 1 });
+        const symbolDictionary = {};
+        // First line is the header
+        for (let i = 1; i < data.length; i++) {
+            const row = data[i];
+            const key = row[2];  // Column C
+            const value = row[1]; // Column B
+            console.log(key, value);
+            if (key && value) {
+                symbolDictionary[key] = value;
+            }
+        }
+        const dictionaryString = JSON.stringify(symbolDictionary, null, 2);
+    
+        return symbolDictionary;
+    },
+    getQuarterEndDate(year, quarter) {
+        let month;
+        switch (quarter) {
+            case 1:
+                month = 3;
+                break;
+            case 2:
+                month = 6;
+                break;
+            case 3:
+                month = 9;
+                break;
+            case 4:
+                month = 12;
+                break;
+            default:
+                throw new Error('Invalid quarter. Quarter should be between 1 and 4.');
+        }
+        const endDate = new Date(year, month, 0);
+        return endDate.toLocaleDateString('en-GB', { day: '2-digit', month: 'short', year: 'numeric' }).replace(/ /g, '-');
+    },
+    
+    quaterlyFinancialResults: async ({ year, quarter, companySymbol }) => {
+        return new Promise((resolve, reject) => {
+            const folderPath = 'src/Financial_result/Quaterly';
+            fs.readdir(folderPath, (err, files) => {
+            if (err) {
+                console.error("Error reading the directory: ", err);
+                reject(err);
+                return;
+            }
+      
+            const file = files.find(file => file.includes(companySymbol));
+            const filePath = `${folderPath}/${file}`;
+      
+            const quaterEndDate = financialFunctions.getQuarterEndDate(year, quarter);
+            console.log(quaterEndDate);
+      
+            const readStream = fs.createReadStream(filePath).pipe(csv());
+      
+            let matchedRow = null;
+            readStream.on('data', (row) => {
+                if (row['PERIOD ENDED'] === quaterEndDate) {
+                    matchedRow = row;
+                }
+            });
+      
+            readStream.on('end', () => {
+                if (matchedRow) {
+                    resolve(matchedRow);
+                } else {
+                    reject(new Error('No matching row found'));
+                }
+            });
+      
+            readStream.on('error', (err) => {
+              reject(err);
+            });
+            });
+        });
+    },
+}
 module.exports = {
     chat,
     generateText,
-    readTextFile
+    readTextFile,
+    financialFunctions,
 };
--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const dotnev = require('dotenv');
 dotnev.config(); // Load the .env file
 
-const { chat } = require('./functions');
-const { generateText, readTextFile } = require('./functions');
+const { chat, generateText, readTextFile, financialFunctions } = require('./functions');
 
 // Initialize the generative model
-const { GoogleGenerativeAI } = require("@google/generative-ai");
+const { GoogleGenerativeAI, FunctionDeclarationSchemaType } = require("@google/generative-ai");
 const generativeAI = new GoogleGenerativeAI( process.env.API_KEY );
 
 
@@ -13,14 +12,73 @@ console.log("Generative AI initialized");
 
 
 
-// Load the prompt from a file and sending it to the generative model
-(async () => {
+async function loadPromptAndSendToModel(filePath, generativeAI) {
     try {
-        const prompt = await readTextFile("./Prompts/prompt.txt");
+        const prompt = await readTextFile(filePath);
         // console.log("Prompt: ", prompt);
         chat(generativeAI, prompt);
         // generateText(generativeAI, prompt);
     } catch (error) {
         console.error("Error reading file:", error);
     }
-})();
+}
+
+// loadPromptAndSendToModel("./Prompts/prompt.txt", generativeAI);
+
+
+// model with functions
+// const financialResultsFunctionDeclaration = {
+//     name: "timeRemaining",
+//     parameters: {
+//         type: "OBJECT",
+//         description: "Calculate the number of days remaining until a future date.",
+//         properties: {
+//             futuredate: {
+//                 type: "STRING",
+//                 description: "The future date to calculate the remaining days for, in a format recognized by the Date constructor.",
+//             },
+//         },
+//         required: ["futuredate"],
+//     },
+// };
+
+
+// const financialResults = {
+//     timeRemaining: async ({ futuredate }) => {
+//         const now = new Date();
+//         const future = new Date(futuredate);
+//         const diff = future - now;
+//         const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+//         return days;
+//     }
+// }
+
+// const modelWithFunctions = generativeAI.getGenerativeModel({
+//     model: "gemini-pro",
+//     tools: {
+//         functionDeclarations : [financialResultsFunctionDeclaration],
+//     },
+// });
+
+// async function chatt() {
+//     const chat = modelWithFunctions.startChat();
+//     const prompt = "Calculate the number of days remaining until 23 december 2025.";
+//     const result = await chat.sendMessage(prompt);
+//     const call = result.response.functionCalls();
+//     console.log(call);
+// }
+
+// console.log(financialResults.timeRemaining({ futuredate: "2025-12-24" }));
+// chatt();
+// (async () => {
+//     const dd = await financialFunctions.quaterlyFinancialResults({ year: 2022, quarter: 1, companySymbol: "ABB" });
+//     console.log(dd);
+// })(); 
+// // dd = await financialFunctions.quaterlyFinancialResults({ year: 2022, quarter: 1, companySymbol: "ABB" });
+financialFunctions.quaterlyFinancialResults({ year: 2022, quarter: 1, companySymbol: 'ABB' })
+    .then(row => {
+        console.log('Matched row:', row);
+    })
+    .catch(err => {
+        console.error('Error:', err);
+});
